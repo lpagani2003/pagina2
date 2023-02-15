5758a76e8beac82878ab10a9138282b1065157cb
lpagani2003 <lpagani2003@gmail.com>
2023-02-15 17:49:01 -0300

    v2

--- templates/js/app/admin.js
+++ templates/js/app/admin.js
@@ -1,4 +1,4 @@
-if (document.getElementById("app")) {
+if (document.getElementById("app1")) {
     const { createApp } = Vue
  
     createApp({
@@ -22,8 +22,8 @@ if (document.getElementById("app")) {
                         this.errored = true
                     })
             },
-            eliminar(producto) {
-                const url = 'http://localhost:5000/pasantes/' + producto;
+            eliminar(pasante) {
+                const url = 'http://localhost:5000/pasantes/' + pasante;
                 var options = {
                     method: 'DELETE',
                 }
@@ -37,5 +37,70 @@ if (document.getElementById("app")) {
         created() {
             this.fetchData(this.url)
         }
-    }).mount('#app')
+    }).mount('#app1')
 }
+if (document.getElementById("app2")) {
+    const { createApp } = Vue
+ 
+    createApp({
+        data() {
+            return {
+                postulantes: [],
+                errored: false,
+                loading: true,
+                url: "http://localhost:5000/postulantes"
+                }
+        },
+        methods: {
+            fetchData(url) {
+                fetch(url)
+                    .then(response => response.json())
+                    .then(data => {
+                        this.postulantes = data;
+                        this.loading = false;
+                    })
+                    .catch(err => {
+                        this.errored = true
+                    })
+            },
+            eliminar(postulante) {
+                const url = 'http://localhost:5000/postulantes/' + postulante;
+                var options = {
+                    method: 'DELETE',
+                }
+                fetch(url, options)
+                    .then(res => res.text()) // or res.json()
+                    .then(res => {
+                        location.reload();
+                    })
+            }
+        },
+        created() {
+            this.fetchData(this.url)
+        }
+    }).mount('#app2')
+}
+function postulante() {
+    var radio = document.getElementById("post");
+    var div1 = document.getElementById("s1");
+    var div2 = document.getElementById("s2");
+    if (radio.checked == true) {
+      div1.style.display = "none";
+      div2.style.display = "block";
+    } else {
+      div1.style.display = "block";
+      div2.style.display = "none";
+    }
+  }
+  function pasante() {
+    var radio = document.getElementById("pasa");
+    var div1 = document.getElementById("s1");
+    var div2 = document.getElementById("s2");
+    if (radio.checked == true) {
+      div1.style.display = "block";
+      div2.style.display = "none";
+    } else {
+      div1.style.display = "none";
+      div2.style.display = "block";
+    }
+  }
---
+++ templates/js/app/update2.js
@@ -0,0 +1,56 @@
+var args = location.search.substr(1).split('&');
+console.log(location.search)
+// lee los argumentos pasados a este formulario
+var parts = []
+for (let i = 0; i < args.length; ++i) {
+    parts[i] = args[i].split('=');
+}
+console.log(args)
+document.getElementById("txtId").value = decodeURIComponent(parts[0][1])
+document.getElementById("txtNombre").value = decodeURIComponent(parts[1][1])
+document.getElementById("txtApellido").value = decodeURIComponent(parts[2][1])
+document.getElementById("txtEmail").value =decodeURIComponent( parts[3][1])
+document.getElementById("txtCV").value =decodeURIComponent( parts[4][1])
+document.getElementById("txtPuesto").value =decodeURIComponent( parts[5][1])
+document.getElementById("txtEstado").value =decodeURIComponent( parts[6][1])
+document.getElementById("txtAnotaciones").value =decodeURIComponent( parts[7][1])
+ 
+function modificar() {
+    let id = document.getElementById("txtId").value
+    let nom = document.getElementById("txtNombre").value
+    let ape = document.getElementById("txtApellido").value
+    let ema = document.getElementById("txtEmail").value
+    let cv = document.getElementById("txtCV").value
+    let puesto = document.getElementById("txtPuesto").value
+    let est = parseInt(document.getElementById("txtEstado").value)
+    let anot = document.getElementById("txtAnotaciones").value
+    let postulante = {
+        nombre: nom,
+        apellido: ape,
+        email: ema,
+        cv: cv,
+        puesto: puesto,
+        estado: est,
+        anotaciones: anot
+    }
+    let url = "http://localhost:5000/postulantes/"+id
+    var options = {
+        body: JSON.stringify(postulante),
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        redirect: 'follow'
+    }
+    fetch(url, options)
+        .then(function () {
+            alert("Registro modificado")
+            window.location.href = "/templates/admin/admin.html";  //NUEVO  
+            // Handle response we get from the API
+        })
+        .catch(err => {
+            //this.errored = true
+            console.error(err);
+            alert("Error al Modificar")
+        })      
+    //window.location.href = "index.html";  //NUEVO 
+ 
+}
